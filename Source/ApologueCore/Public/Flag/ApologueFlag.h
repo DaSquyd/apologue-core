52baff0a61cda263058c0ede83ecb87f75f058b8
DaSquyd <david.jacquish@septyr.com>
2024-09-25 15:59:05 -0700

    Flag and FlagHolderInterface

---
+++ Source/ApologueCore/Public/Flag/ApologueFlag.h
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2024 David Jacquish
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ApologueFlag.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APOLOGUECORE_API UApologueFlag : public UDataAsset
+{
+	GENERATED_BODY()
+};
---
+++ Source/ApologueCore/Public/Flag/ApologueFlagHolderInterface.h
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2024 David Jacquish
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ApologueFlagHolderInterface.generated.h"
+
+class UApologueFlag;
+
+// This class does not need to be modified.
+UINTERFACE()
+class UApologueFlagHolderInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class APOLOGUECORE_API IApologueFlagHolderInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool HasFlag(const TSoftObjectPtr<UApologueFlag>& Flag);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool HasAllFlags(const TArray<TSoftObjectPtr<UApologueFlag>>& Flags);
+	
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool HasAnyFlag(const TArray<TSoftObjectPtr<UApologueFlag>>& Flags);
+};
