79fab7f9f9542b4618ed8fb82600ec510015e469
DaSquyd <30611805+DaSquyd@users.noreply.github.com>
2025-03-28 17:54:27 -0700

    Update ApologueEventContext.h

--- Source/ApologueCore/Public/Event/ApologueEventContext.h
+++ Source/ApologueCore/Public/Event/ApologueEventContext.h
@@ -16,10 +16,18 @@ class APOLOGUECORE_API UApologueEventContext : public UObject
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category="Apologue Event Context", meta=(AllowPrivateAccess))
+	UPROPERTY(BlueprintReadOnly, Category="Apologue Event Context", meta=(AllowPrivateAccess))
 	bool bIsCanceled = false;
+
+	UPROPERTY(BlueprintReadOnly, Category="Apologue Event Context", meta=(AllowPrivateAccess))
+	int32 Depth = 0;
 
 public:
 	bool IsCanceled() const { return bIsCanceled; }
 	void Cancel() { bIsCanceled = true; }
+
+	int32 GetDepth() const { return Depth; }
+
+	UFUNCTION(BlueprintCallable, Category="Apologue Event Context")
+	UPARAM(DisplayName="New Depth") int32 IncrementDepth() { return ++Depth; }
 };
