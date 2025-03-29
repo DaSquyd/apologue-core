1d8f2b707bc23e2a5c78d9a9641754c4f6c66f21
DaSquyd <30611805+DaSquyd@users.noreply.github.com>
2025-03-28 18:03:25 -0700

    Update ApologueStat.h
    display name options

--- Source/ApologueCore/Public/Stat/ApologueStat.h
+++ Source/ApologueCore/Public/Stat/ApologueStat.h
@@ -17,14 +17,29 @@ class APOLOGUECORE_API UApologueStat final : public UDataAsset
 {
 	GENERATED_BODY()
 
+	// Good for flavor text and descriptions
+	// Examples: "Strength", "Magic Defense", "Speed"
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Apologue Stat", meta=(AllowPrivateAccess))
+	FText VerboseDisplayName;
+
+	// Good for UI
+	// Examples: "Strength", "Mg. Defense", "Speed"
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Apologue Stat", meta=(AllowPrivateAccess))
 	FText DisplayName;
 
+	// Useful in places where you can only fit very small texts
+	// Examples: "STR", "MgD", "SPE"
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Apologue Stat", meta=(AllowPrivateAccess))
+	FText AbbreviatedName;
+
+	// Used to calculate the active stat value with provided context
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Instanced, Category="Apologue Stat", meta=(AllowPrivateAccess, ShowOnlyInnerProperties))
 	TObjectPtr<UApologueStatFunction> Function;
 	
 public:
+	const FText& GetVerboseDisplayName() const { return VerboseDisplayName; }
 	const FText& GetDisplayName() const { return DisplayName; }
+	const FText& GetAbbreviatedName() const { return AbbreviatedName; }
 	
 	UFUNCTION(BlueprintPure, Category="Apologue Stat")
 	int32 GetValue(const int32 BaseValue, const UApologueStatFunctionContext* Context) const;
