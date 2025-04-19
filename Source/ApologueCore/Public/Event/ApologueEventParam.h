bb9a1a138f90a161246e8fcc18512ea37569072f
DaSquyd <30611805+DaSquyd@users.noreply.github.com>
2025-04-18 20:00:31 -0700

    ApologueEventCallbackParam renamed
    now ApologueEventParam

---
+++ Source/ApologueCore/Public/Event/ApologueEventParam.h
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2024 David Jacquish
+
+#pragma once
+
+#include "ApologueEvent.h"
+#include "ApologueEventBroadcasterInterface.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ApologueEventParam.generated.h"
+
+DECLARE_DYNAMIC_DELEGATE(FApologueEventCompleteCallback);
+DECLARE_DYNAMIC_DELEGATE_ThreeParams(FApologueEventCallback, TScriptInterface<IApologueEventBroadcasterInterface>, EventBroadcaster, const UApologueEventContext*, Context, bool&, bIsDeferred);
+
+USTRUCT(BlueprintType)
+struct APOLOGUECORE_API FApologueEventParam
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Apologue Event Param")
+	TSoftObjectPtr<UApologueEvent> Event;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Apologue Event Param")
+	FApologueEventCallback Callback;
+
+	bool IsValid() const
+	{
+		return !Event.IsNull() && Callback.IsBound();
+	}
+};
+
+UCLASS()
+class APOLOGUECORE_API UApologueEventParamLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, Category="Apologue|Event|Event Param")
+	static bool IsValid(const FApologueEventParam& EventParam)
+	{
+		return EventParam.IsValid();
+	}
+};
